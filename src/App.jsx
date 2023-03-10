dea5e5915f63216c938e7d3353d633a0ed0e8d59
Augusto Santillan <agusantillan@hotmail.com>
2023-03-10 19:36:16 -0300

    update app - layouts y dependencias

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { MainLayout } from "./layouts";
+
+function App() {
+  return (
+    <>
+      <MainLayout>
+        <h1>hola</h1>
+      </MainLayout>
+    </>
+  );
+}
+
+export default App;
