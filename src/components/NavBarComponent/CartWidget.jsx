bd78de0837bc67ef61e844d03b5ed3efe873bf1c
Augusto Santillan <agusantillan@hotmail.com>
2023-03-25 23:12:04 -0300

    update: se agrego CartWidget

---
+++ src/components/NavBarComponent/CartWidget.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button, Badge } from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
+
+// carrito de compras, con numero de items harcodeado 
+
+export const CartWidget = ({countCart}) => {
+  return (
+    <Button variant="outline-light p-2 ">
+      <FaShoppingCart className="mx-1" />
+      <Badge pill bg="secondary mx-1">
+        {countCart}
+      </Badge>
+    </Button>
+  );
+};
--- src/components/NavBarComponent/NavBarComponent.jsx
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import { NavItem } from "./NavItem";
+import { CartWidget } from "./CartWidget";
 
 export const NavBarComponent = () => {
+
+
   return (
     <Navbar expand="md" variant="dark" bg="dark">
-      <Navbar.Brand href="#" style={{margin: 6}}> Camisetas Trend</Navbar.Brand>
+      <Navbar.Brand href="/" style={{margin: 6}}> Camisetas Trend</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse>
         <Nav >
@@ -15,7 +18,9 @@ export const NavBarComponent = () => {
           <NavItem src="/rugby" label="Rugby" />
           <NavItem src="/f1" label="Fórmula 1" />
         </Nav>
+      <CartWidget countCart="2" />
       </Navbar.Collapse>
+      
     </Navbar>
   );
 };
